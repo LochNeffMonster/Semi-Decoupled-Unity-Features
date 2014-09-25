a39eb4429c5e69953a89a0eb7477cc8466da5cf2
LochNeffMonster <atneff@ucsc.edu>
2014-09-25 11:23:49 -0700

    added objective class

---
+++ Semi-Decoupled-Unity-Features/Assets/Scripts/GUI and Displays/Objective.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objective : MonoBehaviour
+{
+
+    public string objective = null;
+    public int objectiveOrder = 0;
+
+    private bool _objectiveSet = false;
+
+    public string getObjective()
+    {
+        return objective;
+    }
+
+    public int getOrder()
+    {
+        return objectiveOrder;
+    }
+
+    public bool isObjectiveSet()
+    {
+        return _objectiveSet;
+    }
+
+    [RPC]
+    public void setObjective()
+    {
+        _objectiveSet = true;
+    }
+}
